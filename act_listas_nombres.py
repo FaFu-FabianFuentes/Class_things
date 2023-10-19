3c54a9fe390f5739be24ea6a037310f94a21c1dd
FaFu-FabianFuentes <147836462+FaFu-FabianFuentes@users.noreply.github.com>
2023-10-19 11:59:04 -0300

    Add files via upload

--- act_listas_nombres.py
+++ act_listas_nombres.py
@@ -1,16 +1,36 @@
 import os
 import time
 os.system("cls")
-
+opc_menu=("")
 n=[]
+while True :
+    opc_menu = str(input('''
+                Sistema de nombres :
+                
+                1-ingresar nombres
+                2-revisar datos ingresados
+                3-buscar nombre en lista
+                4 salir
+                
+                '''))
+    if opc_menu == 1:
+        while (len(n)) <= 6:
+            print("ingrese 6 nombres:")
+            n.append(input(""))
 
-while (len(n)) <= 6:
-    print("ingrese nombre:")
-    n.append(input(""))
+    if opc_menu == 2:
+        print("nombres ingresados :")
+        for i in range(len(n)):
+            time.sleep(1)
+            n.sort()
+            print(n[i])
+
+    if opc_menu == 3:
+        print("ingrese cual numero de la lsita desea buscar :")
+        print(n[i-1])
+
+    if opc_menu == 4:
+        break
+        
 
 
-print("nombres ingresados :")
-for i in range(len(n)):
-    time.sleep(1)
-    n.sort()
-    print(n[i])
